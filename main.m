085c582644b920210834d23cc7110e4293a71174
sorona <sorona@t2.technion.ac.il>
2014-07-25 16:56:03 +0300

    ready for simul

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
-N = 20;
+N = 40;
 PSNR  = linspace(25,35,N);
 BPP   = zeros(size(PSNR));
 rPSNR = zeros(size(PSNR));
 for i=1:N
     [rPSNR(i),BPP(i)] = Pmain(PSNR(i));
 end
-save('18DictRes.mat','BPP','rPSNR');
+save('dataSimul/18DictRes.mat','BPP','rPSNR');
 % figure;plot(BPP,rPSNR);
